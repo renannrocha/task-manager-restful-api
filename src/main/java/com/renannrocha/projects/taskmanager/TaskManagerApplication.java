285361b2e2fd9d25ed056ae4966c5872de300a23
renannrocha <renanrocha8897@gmail.com>
2024-03-04 22:31:44 -0300

    profile test configuration - compilation testing

--- src/main/java/com/renannrocha/projects/taskmanager/TaskManagerApplication.java
+++ src/main/java/com/renannrocha/projects/taskmanager/TaskManagerApplication.java
@@ -1,10 +1,19 @@
 package com.renannrocha.projects.taskmanager;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class TaskManagerApplication {
+
+	@Bean
+	public CommandLineRunner executar(){
+		return args -> {
+			System.out.println("deu certo");
+		};
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(TaskManagerApplication.class, args);
