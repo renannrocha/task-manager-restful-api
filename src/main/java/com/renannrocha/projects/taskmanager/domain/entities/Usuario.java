e557d354713d185bb145812155c81e2f7342292b
renannrocha <renanrocha8897@gmail.com>
2024-03-13 08:28:36 -0300

    refactoring user

--- src/main/java/com/renannrocha/projects/taskmanager/domain/entities/Usuario.java
+++ src/main/java/com/renannrocha/projects/taskmanager/domain/entities/Usuario.java
@@ -21,10 +21,4 @@ public class Usuario implements Serializable {
     private String nome;
     private String email;
     private String senha;
-
-    public Usuario(String nome, String email, String senha) {
-        this.nome = nome;
-        this.email = email;
-        this.senha = senha;
-    }
 }
