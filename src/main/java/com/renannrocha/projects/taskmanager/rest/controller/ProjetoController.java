1a60f9675ed9ef6c18cee2d0d2a5e877eec60430
renannrocha <renanrocha8897@gmail.com>
2024-03-22 08:39:55 -0300

    add all entities controllers

--- src/main/java/com/renannrocha/projects/taskmanager/rest/controller/ProjetoController.java
+++ src/main/java/com/renannrocha/projects/taskmanager/rest/controller/ProjetoController.java
@@ -1,16 +1,14 @@
 package com.renannrocha.projects.taskmanager.rest.controller;
 
-import com.renannrocha.projects.taskmanager.domain.repositories.UsuarioRepository;
+import com.renannrocha.projects.taskmanager.domain.repositories.ProjetoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping(value = "/api/usuarios")
-public class UsuarioController {
+@RequestMapping(value = "/api/projetos")
+public class ProjetoController {
 
     @Autowired
-    private UsuarioRepository repository;
-
+    private ProjetoRepository repository;
 }
-
--- src/main/java/com/renannrocha/projects/taskmanager/rest/controller/TarefaController.java
+++ src/main/java/com/renannrocha/projects/taskmanager/rest/controller/TarefaController.java
@@ -1,16 +1,15 @@
 package com.renannrocha.projects.taskmanager.rest.controller;
 
-import com.renannrocha.projects.taskmanager.domain.repositories.UsuarioRepository;
+import com.renannrocha.projects.taskmanager.domain.repositories.TarefaRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping(value = "/api/usuarios")
-public class UsuarioController {
+@RequestMapping(value = "/api/tarefas")
+public class TarefaController {
 
     @Autowired
-    private UsuarioRepository repository;
+    private TarefaRepository repository;
 
 }
-
