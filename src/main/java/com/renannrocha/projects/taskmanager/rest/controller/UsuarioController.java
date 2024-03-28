c4d49c94ac6b32fa0983cb5077c77dd74b569a89
renannrocha <renanrocha8897@gmail.com>
2024-03-27 23:14:58 -0300

    implementation UsuarioController whith JpaRepository

--- src/main/java/com/renannrocha/projects/taskmanager/rest/controller/UsuarioController.java
+++ src/main/java/com/renannrocha/projects/taskmanager/rest/controller/UsuarioController.java
@@ -1,9 +1,15 @@
 package com.renannrocha.projects.taskmanager.rest.controller;
 
+import com.renannrocha.projects.taskmanager.domain.entities.Usuario;
 import com.renannrocha.projects.taskmanager.domain.repositories.UsuarioRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.data.domain.Example;
+import org.springframework.data.domain.ExampleMatcher;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.util.List;
 
 @RestController
 @RequestMapping(value = "/api/usuarios")
@@ -12,5 +18,41 @@ public class UsuarioController {
     @Autowired
     private UsuarioRepository repository;
 
+    @GetMapping(value = "/{id}")
+    public Usuario getUsuarioById(@PathVariable Integer id){
+        return repository.findById(id).orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Cliente não Encontrado"));
+    }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public Usuario save( @RequestBody Usuario usuario){
+        return repository.save(usuario);
+    }
+
+    @DeleteMapping(value = "/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void delete(@PathVariable Integer id){
+        repository.findById(id).map(usuario -> {
+           repository.delete(usuario);
+           return usuario;
+        }).orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Cliente não encontrado"));
+    }
+
+    @PutMapping(value = "/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void update (@PathVariable Integer id, @RequestBody Usuario usuario){
+        repository.findById(id).map(usuarioExistente -> {
+            usuario.setId(usuarioExistente.getId());
+            repository.save(usuario);
+            return usuarioExistente;
+        }).orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Cliente não encontrado"));
+    }
+
+    @GetMapping
+    public List<Usuario> find(Usuario filtro){
+        ExampleMatcher matcher = ExampleMatcher.matching().withIgnoreCase().withStringMatcher(ExampleMatcher.StringMatcher.CONTAINING);
+        Example example = Example.of(filtro, matcher);
+        return repository.findAll(example);
+    }
 }
 
